aab17f388bc5dd265e71037ff7fb683061021e0c
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-01-30 17:20:02 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if(feet < 400){
+    greeting = 'This one is on me!';
+  } else if (feet <= 2000 ) {
+    greeting = 'That will be twenty bucks.'
+  } else if (feet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  } else {
+    greeting = 'No can do.'
+  }
+  console.log(greeting);
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let cityGreeting;
+  city === 'NYC' ? (cityGreeting = 'Ok, sounds good.') : (cityGreeting = 'No go.')
+  return cityGreeting; 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipGenerosity){
+  let tipResponse;
+  switch(tipGenerosity) {
+
+    case 'generous':
+      tipResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipResponse = 'Thank you.'
+      break;
+    default:
+      tipResponse = 'Bye.'    
+      break;
+  }
+
+  console.log(tipGenerosity + "-->" + tipResponse);
+
+  return tipResponse;
 }
+const tipGenerosity = 'generous';
+switchOnCharmFromTip(tipGenerosity);
